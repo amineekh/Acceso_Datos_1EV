1205bf71c9e33569ffc1ac67721faead8e6381bf
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2023-09-26 13:16:39 +0200

    19 sep 2023

--- src/Ejercicio_Repaso/Ejercicio_5.java
+++ src/Ejercicio_Repaso/Ejercicio_5.java
@@ -4,13 +4,33 @@ import java.util.Scanner;
 
 public class Ejercicio_5 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+            // (int) para convertir el numero deciamal en un numero entero
+            //  (Math.random()*100) para genar numeros aleaotrios de 0-100 por eso se multiplica *100
+            int aleaotrio = (int) (Math.random()*100);
 
+            Scanner sc = new Scanner(System.in);
+            int numero = 0;
+            int intento = 0;
 
+            while(aleaotrio != numero){
 
+                intento++;
 
+                System.out.println("introduzca un numero");
+                numero = sc.nextInt();
 
+                if (aleaotrio < numero){
+                    System.out.println("MAS BAJO !!!");
+                }
+                else if (aleaotrio > numero){
+                    System.out.println("MAS ALTO !!!");
+                }
+                else {
+                    System.out.println("El numero aleaotrioa era " + aleaotrio + "  ES CORECTO !!!");
+                    System.out.println("el numero de intentos es: " + intento);
+                }
+            }
 
     }
 }
