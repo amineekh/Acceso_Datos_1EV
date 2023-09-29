15df0f177a53c8cb267041c53fd175b9740a0415
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2023-09-29 11:02:38 +0200

    29 sep 2023 OLA

--- src/Manejo_ficheros/Ejercicio_7.java
+++ src/Manejo_ficheros/Ejercicio_7.java
@@ -11,17 +11,28 @@ public class Ejercicio_7 {
         // Ejercicio 7
         //Utiliza la librería FileUtils para copiar dos ficheros
 
+
+
+        // PASO 1: COGEMOS LAS RUTAS DE LOS FICHEROS   QUE QUEREMOS COPIAR Y LOS ALMACENAMOS EN UNA VARIABLE
+        // TAMBIEN SE PUEDEN CREAR LOS FICHEROS MEDIANTE EL CODIGO O MEDIANTE LA INTERFAZ GRAFICA
         String RutaOrigen = "C:\\Users\\MohamedAmineElkhayya\\IdeaProjects\\Acceso_Datos_1EV\\NuevoDIR\\EJEMPLO-1.txt";
+
+        //// PASO 2: ALMACENAMOS EN UNA VARIABLE LA RUUTA DESTINO DONDE SE VAN A COPIAR LOS FICHEROS
         String RutaDestino = "C:\\Users\\MohamedAmineElkhayya\\IdeaProjects\\Acceso_Datos_1EV";
 
+        // PASO 3: Creamos objetos File para representar los archivos de origen y el directorio de destino
         File file_ORIGEN = new File(RutaOrigen);
         File file_DESTINO = new File(RutaDestino);
 
-
+        // PASO 4: Intentamos mover los archivos al directorio de destino
         try {
+            // Copiamos archivo a la carpeta de destino CON LA SIGUIENTE CLASE:
+            // Primero ponemos fichero que copiamos y después la variable donde esta almacenada ruta del destino
             FileUtils.copyFileToDirectory(file_ORIGEN, file_DESTINO);
             System.out.println("Fichero copiado");
+
         } catch (IOException ex) {
+            // En caso de error, mostramos un mensaje de error y la traza de excepción.
             System.err.println("Error al copiar el archivo");
             ex.printStackTrace();
 
--- src/Manejo_ficheros/Ejercicio_8.java
+++ src/Manejo_ficheros/Ejercicio_8.java
@@ -11,24 +11,33 @@ public class Ejercicio_8 {
         //Ejercicio 8
         //Utiliza la librería FileUtils para mover dos ficheros
 
+        // PASO 1: COGEMOS LAS RUTAS DE LOS FICHEROS   QUE QUEREMOS MOVER Y LOS ALMACENAMOS EN UNA VARIABLE
+        // TAMBIEN SE PUEDEN CREAR LOS FICHEROS MEDIANTE EL CODIGO O MEDIANTE LA INTERFAZ GRAFICA
         String RutaOrigen = "C:\\Users\\MohamedAmineElkhayya\\IdeaProjects\\Acceso_Datos_1EV\\NuevoDIR\\EJEMPLO-1.txt";
         String RutaOrigen2 = "C:\\Users\\MohamedAmineElkhayya\\IdeaProjects\\Acceso_Datos_1EV\\NuevoDIR\\EJEMPLO-2.txt";
 
+        //// PASO 2: ALMACENAMOS EN UNA VARIABLE LA RUUTA DESTINO DONDE SE VAN A MOVER LOS FICHEROS
         String RutaDestino = "C:\\Users\\MohamedAmineElkhayya\\IdeaProjects";
 
 
+        // PASO 3: Creamos objetos File para representar los archivos de origen y el directorio de destino
         File file_ORIGEN = new File(RutaOrigen);
         File file_ORIGEN_2 = new File(RutaOrigen2);
         File file_DESTINO = new File(RutaDestino);
 
 
+        // PASO 4: Intentamos mover los archivos al directorio de destino
         try {
+            // Movemos el PRIMER archivo a la carpeta de destino CON LA SIGUIENTE CLASE:
             FileUtils.moveFileToDirectory(file_ORIGEN , file_DESTINO, true);
             System.out.println("Fichero movido");
 
+            // Movemos el SEGUNDO archivo a la carpeta de destino CON LA SIGUIENTE CLASE:
             FileUtils.moveFileToDirectory( file_ORIGEN_2 ,file_DESTINO, true);
             System.out.println("Fichero movido");
+
         } catch (IOException ex) {
+            // En caso de error, mostramos un mensaje de error y la traza de excepción.
             System.err.println("Error al mover el archivo");
             ex.printStackTrace();
 
