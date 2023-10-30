71539af36c66354e317bcb310ad7d6593d6aa1af
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2023-10-30 08:52:07 +0100

    30 oct 2023

--- src/AD_ficheros_OBJETO/ficheros_Binarios_Persona.java
+++ src/AD_ficheros_OBJETO/ficheros_Binarios_Persona.java
@@ -6,42 +6,24 @@ import java.io.*;
 public class ficheros_Binarios_Persona {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int opcion=0;
         // Creación de un objeto de tipo File con el nombre de archivo "persona12"
-        File file = new File("persona.txt");
-        do {
-            System.out.println("0.salir programa");
-            System.out.println( "1. función para guardar un objeto Persona en un fichero con el nombre persona1");
-            System.out.println("2.función para recuperar un objeto Persona del fichero persona1 ");
-            System.out.println("introduzca una opcion");
-            opcion=sc.nextInt();
-            switch (opcion){
-                case 0:
-                    System.out.println("gracias por usar el programa");
-                    break;
-                case 1:
-                    // Llamada al método guardarObjeto con el objeto file como argumento
-                    guardarObjeto(file);
-                    break;
-                case 2:
-                    // Llamada al método recuperarObjeto con el objeto file como argumento
-                    recuperarObjeto(file);
-                    break;
-            }
-        }while(opcion!=0);
-
+        File file = new File("persona_messi");
+        // Llamada al método guardarObjeto con el objeto file como argumento
+        guardarObjeto(file);
+        // Llamada al método recuperarObjeto con el objeto file como argumento
+        recuperarObjeto(file);
     }
 
     public static void guardarObjeto(File file) {
         try {
             // Creación de un flujo de salida de bytes hacia el archivo
+            //El objeto fileOut se utiliza para escribir bytes en el archivo.
             FileOutputStream fileOut = new FileOutputStream(file);
             // Creación de un ObjectOutputStream que se conecta al flujo de salida de bytes
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
 
             // Creación de unA INSTANCIA objeto de la clase Persona
-            Persona persona = new Persona(1, "NombrePersona", 30, "123456789A");
+            Persona persona = new Persona(1, "messi", 30, "123456789A");
 
             // Escribir el objeto persona en el flujo de salida
             out.writeObject(persona);
@@ -57,7 +39,7 @@ public class ficheros_Binarios_Persona {
 
     public static void recuperarObjeto(File file) {
         try {
-            // Creación de un flujo de entrada de bytes desde el archivo
+            // Creación de un flujo de entrada de bytes desde el archivo: El objeto fileIn se utiliza para leer los bytes del archivo.
             FileInputStream fileIn = new FileInputStream(file);
             // Creación de un ObjectInputStream que se conecta al flujo de entrada de bytes
             ObjectInputStream in = new ObjectInputStream(fileIn);
