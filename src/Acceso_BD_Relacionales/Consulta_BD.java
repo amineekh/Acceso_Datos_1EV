9e6c752f4321527dd8b5612e0926efbad89cf804
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2023-11-24 10:35:21 +0100

    24 - 11 - 2023 insertar datos sql

--- src/Acceso_BD_Relacionales/Consulta_BD.java
+++ src/Acceso_BD_Relacionales/Consulta_BD.java
@@ -7,7 +7,7 @@ public class Consulta_BD {
 
     public static Connection conn= null;
 
-
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws SQLException {
         Scanner sc = new Scanner(System.in);
         int opcion=0;
@@ -18,6 +18,8 @@ public class Consulta_BD {
             System.out.println("1. conectar SGBD");
             System.out.println("2. Procede a mostrar todos registros correspondientes a los campos: emp_no, oficio y dept_no");
             System.out.println("3. Repite el paso anterior mostrando además el campo dnombre");
+            System.out.println("4. Insertar datos en la tabla departamentos");
+            System.out.println("5. Insertar datos en la tabla empleados");
 
             System.out.println("introduzca una opcion: ");
             opcion =sc.nextInt();
@@ -35,8 +37,50 @@ public class Consulta_BD {
                 case 3:
                     mostrar_consulta_dos_tablas();
                     break;
+                case 4:
+                    insertar_datos_tabla_departamentos();
+                case 5:
+                    insertar_datos_tabla_empledos();
+                    break;
             }
         }while(opcion != 0);
+
+    }
+
+    private static void insertar_datos_tabla_departamentos() {
+    }
+
+    private static void insertar_datos_tabla_empledos() throws SQLException {
+        asignar_bd();
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO empleados VALUES(?,?,?,?,?,?,?,?)");
+        System.out.println("introduzca el emp_no");
+        int emp_no=sc.nextInt();
+        System.out.println("introduzca apellido");
+        String apellido= sc.next();
+        System.out.println("introduzca oficio");
+        String oficio = sc.next();
+        System.out.println("introduzca dir");
+        int dir= sc.nextInt();
+        System.out.println("introduzca fecha");
+        String fecha_alt= sc.next();
+        System.out.println("introzca salario");
+        float salario = sc.nextFloat();
+        System.out.println("introzca comisiion");
+        float comision = sc.nextFloat();
+        System.out.println("introduzca dept_no");
+        int dept_no_empleados= sc.nextInt();
+
+        ps.setInt(1, emp_no);
+        ps.setString(2, apellido);
+        ps.setString(3, oficio);
+        ps.setInt(4, dir);
+        ps.setString(5, fecha_alt);
+        ps.setFloat(6, salario);
+        ps.setFloat(7, comision);
+        ps.setInt(8, dept_no_empleados);
+        ps.executeUpdate();
+        System.out.println("registro insertado correctamente");
 
     }
 
@@ -81,7 +125,7 @@ public class Consulta_BD {
         }
 
         System.out.println("");
-        System.out.println("ahi estan los datos de la tabla: empleados");
+        System.out.println("ahi estan las columnas de la tabla: empleados");
         System.out.println("");
     }
     private static void asignar_bd() throws SQLException {
